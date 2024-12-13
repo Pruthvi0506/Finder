5b037cd8aa70aa8a62d7de6daf036530af28a97b
Pruthvi0506 <gangishettypruthvi@gmail.com>
2024-12-12 20:49:09 -0600

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
-
-  $con = mysqli_connect("127.0.0.1","root","","ft");
+$con = mysqli_connect("l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","r0ivd7ouzzx42r4m","rgxtflx0oblzqv2n","irxtychekmkg93c8");
+  //$con = mysqli_connect("127.0.0.1","root","","ft");
   if (mysqli_connect_errno()){
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
